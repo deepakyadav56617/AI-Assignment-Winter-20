f1e1e98137cfd2ca0524f06eea5c3edfdc73e9d0
deepakyadav56617 <50566143+deepakyadav56617@users.noreply.github.com>
2020-07-22 13:12:57 +0530

    Update 8QueenA*.cpp

--- 8QueenA*/8QueenA*.cpp
+++ 8QueenA*/8QueenA*.cpp
@@ -1,19 +1,6 @@
 /*
 NAME:DEEPAK KUMAR YADAV
-ROLL_NO:1801048
-1.State space:
-       Here sate space is a 8x8 matrix where the position of a queen is represented by 1
-2.State transition operator:
-		adding a new queen which is denoted by 1 in the subsequent row.
-3.intitial state:
-		a 8 x 8 null matrix.
-4.Goal state:
-		When all the rows are filled with a single queen at non attacking positions,
-5.Cost: the total cost i got with my approach was 803.
-6.I implemented the heuristic with A* search implementation the cost i reduced greatly from previous assignemnt
-  bfs cost of 2056 to heuristic search cost 803.		 				
-		       
-*/
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
